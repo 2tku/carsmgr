6d736072c7a0c3ea3679bd8a14f4793115c5cc61
thuattq1@ViettelGroup.ViettelAD.com <thuattq1@ViettelGroup.ViettelAD.com>
2017-12-04 17:50:09 +0700

    add view addTask

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-angular.module('AppModule', ['ngRoute', 'ngResource'])
+angular.module('AppModule', ['ngRoute', 'ngResource', 'ngAnimate', 'ngSanitize', 'ui.bootstrap'])
 .factory('Tasks', ['$resource', function($resource){
         return $resource('/tasks/:id', null, {
             'update': { method:'PUT' }
@@ -34,12 +34,97 @@ angular.module('AppModule', ['ngRoute', 'ngResource'])
 .controller("TasksViewCtrl", ['$scope', '$routeParams', 'Tasks', '$location',
     function ($scope, $routeParams, Tasks, $location) {
         $scope.tasks = Tasks.query();
+
+        $scope.showAddNew = function() {
+            $location.url('/add');
+        }
     }
 ])
 // create the controller and inject Angular's $scope
 .controller("TaskEditCtrl", ['$scope', '$routeParams', 'Tasks', '$location',
     function ($scope, $routeParams, Tasks, $location) {
+        $scope.task = Tasks.get({id: $routeParams.id });
 
+        $scope.save = function() {
+            if(!$scope.task) return;
+            //var todo = new Todos({ name: $scope.newTodo, completed: false });
+
+            $scope.task.$save(function(){
+                Tasks.push($scope.task);
+                $location.url('/');
+            });
+        }
+
+        $scope.cancel = function() {
+            $scope.task = null;
+            $location.url('/view');
+        }
+    }
+])
+.controller("TaskAddCtrl", ['$scope', '$routeParams', 'Tasks', '$location',
+    function ($scope, $routeParams, Tasks, $location) {
+        /* config datetime picker [ */
+        this.dateOptions = {
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+        };
+
+        this.open1 = () => {this.popup1.opened = true;};
+        this.popup1 = {opened: false};
+
+        this.open2 = () => {this.popup2.opened = true;};
+        this.popup2 = {opened: false};
+
+        this.open3 = () => {this.popup3.opened = true;};
+        this.popup3 = {opened: false};
+
+        this.altInputFormats = ['M!/d!/yyyy'];
+        this.dateTimeFormat = 'dd/MM/yyyy';
+        /*] config datetime picker  */
+
+        this.lstOwnOrg = ['Nội bộ', 'Bên ngoài'];
+        this.lstProcessType =  ['SCN', 'SCC'];
+
+        this.createDate = new Date();
+        this.vehicle = 'XeOto';
+        this.ownOrg = '';
+        this.processType = '';
+        this.tastContent = '';
+        this.beginTime = new Date();
+        this.endTime = null;
+        this.taskRealHour = 0;
+        this.waitMaterialHour = 0;
+        this.km = 0;
+        this.note = '';
+
+        this.save = function() {
+            if(!this.vehicle || !this.createDate) return;
+            var newTask = new Tasks({ 
+                create_date         : this.createDate,
+                vehicle             : this.vehicle,
+                own_org             : this.ownOrg,
+                process_type        : this.processType,
+                tast_content        : this.tastContent,
+                begin_time          : this.beginTime,
+                end_time            : this.endTime,
+                task_real_hour      : this.taskRealHour,
+                wait_material_hour  : this.waitMaterialHour,
+                km                  : this.km,
+                note                : this.note
+            });
+
+            console.log(newTask);
+            newTask.$save(function(){
+                Tasks.push(newTask);
+                $location.url('/');
+            });
+        }
+
+        this.cancel = function() {
+            $location.url('/view');
+        }
     }
 ])
 // configure our routes
@@ -52,6 +137,10 @@ angular.module('AppModule', ['ngRoute', 'ngResource'])
         .when('/edit', {
             templateUrl : '/taskEdit.html',
             controller  : 'TaskEditCtrl'
+        })
+        .when('/add', {
+            templateUrl : '/taskAdd.html',
+            controller  : 'TaskAddCtrl'
         })
         .when('/view', {
             templateUrl : '/tasksView.html',
